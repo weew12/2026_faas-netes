15a6b8b949266096fea961eeba5c32d1588e909b
weew12 <weew12@2528566339@qq.com>
2026-04-01 22:40:37 +0800

    3 注释 version

--- version/version.go
+++ version/version.go
@@ -1,20 +1,22 @@
 // License: OpenFaaS Community Edition (CE) EULA
 // Copyright (c) 2017,2019-2024 OpenFaaS Author(s)
 
+// package version 提供版本管理功能，用于获取组件的版本号、Git提交信息
 package version
 
 var (
-	// Version release version of the provider
+	// Version 组件正式发布版本号
 	Version string
 
-	// GitCommit SHA of the last git commit
+	// GitCommit 最后一次Git提交的SHA哈希值
 	GitCommit string
 
-	// DevVersion string for the development version
+	// DevVersion 开发环境默认版本标识
 	DevVersion = "dev"
 )
 
-// BuildVersion returns current version of the provider
+// BuildVersion 构建并返回当前组件版本
+// 未设置正式版本时返回开发版本标识
 func BuildVersion() string {
 	if len(Version) == 0 {
 		return DevVersion
@@ -22,7 +24,8 @@ func BuildVersion() string {
 	return Version
 }
 
-// GetReleaseInfo includes the SHA and the release version
+// GetReleaseInfo 获取组件完整发布信息
+// 返回：sha=Git提交哈希，release=组件版本
 func GetReleaseInfo() (sha, release string) {
 	return GitCommit, BuildVersion()
 }
